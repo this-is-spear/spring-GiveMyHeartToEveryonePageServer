394e8f09e77e3fd8f1de1692ed7ad423439894b4
ThisIsSpear <rjsckdd12@gmail.com>
2021-11-30 15:07:11 +0900

    upload sample image
    upload sample image

---
+++ src/test/java/com/givehearttoeveryone/server/GiveMyHeartToEveryoneWebPage/GiveMyHeartToEveryoneWebPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.givehearttoeveryone.server.GiveMyHeartToEveryoneWebPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiveMyHeartToEveryoneWebPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
